daf8e86c524d59b9c8a7df7a7b4462037b5f0f08
Cerulean <tahir.naseer@hotmail.fr>
2021-04-15 13:34:59 +0200

    ajouts de plus de tests

--- exePoint.js
+++ exePoint.js
@@ -2,4 +2,9 @@ const Point = require('./Point.js');
 
 let p1 = new Point(4, 5);
 let p2 = new Point(1, 8);
-console.log(p1.toString());
+p1.toString();
+p2.toString();
+
+p1.setX(10);
+p1.setY(52);
+p1.toString();
