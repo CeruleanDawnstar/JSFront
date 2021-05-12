4e3232ebf87dd0764a66ca8532f6edc3cde49a9a
CeruleanDawnstar <68052140+CeruleanDawnstar@users.noreply.github.com>
2021-05-12 11:24:46 +0200

    Delete exeHippo.js

--- exeHippo.js
+++
@@ -1,9 +0,0 @@
-const Hippo = require('./Hippopotamus.js');
-
-let Hippo1 = new Hippo('Lars', 1000000, 20);
-let Hippo2 = new Hippo('Ozymandias', 90000, 21);
-
-Hippo1.toString();
-Hippo2.toString();
-
-Hippo1.fight(Hippo2);
