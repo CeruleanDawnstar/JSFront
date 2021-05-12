048200c3effef9b45ff947c47000393cbb00b6a9
CeruleanDawnstar <68052140+CeruleanDawnstar@users.noreply.github.com>
2021-05-12 11:24:35 +0200

    Delete exeCircle.js

--- exeCircle.js
+++
@@ -1,8 +0,0 @@
-const Circle = require('./Circle.js');
-const Point = require('./Point.js');
-
-let p2 = new Point(1,2);
-let circle = new Circle(p2, 7);
-
-circle.area();
-circle.containsPoint(p2);
